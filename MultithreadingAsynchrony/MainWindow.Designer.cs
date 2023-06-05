27dca1e8b3a6a858ba5fc2010af209f5e371a121
Viktor Velychko <Cezn.Vi@gmail.com>
2023-06-05 21:29:38 +0300

    Work in progress

--- MultithreadingAsynchrony/MainWindow.Designer.cs
+++ MultithreadingAsynchrony/MainWindow.Designer.cs
@@ -29,32 +29,97 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
-            button1 = new Button();
+            button_GenereteSimlyInt = new Button();
+            textBox_MinValue = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox_MaxValue = new TextBox();
+            listBox_ViewResult = new ListBox();
             SuspendLayout();
             // 
-            // button1
+            // button_GenereteSimlyInt
             // 
-            button1.Location = new Point(12, 49);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            button_GenereteSimlyInt.BackColor = Color.LightGray;
+            button_GenereteSimlyInt.Location = new Point(168, 85);
+            button_GenereteSimlyInt.Name = "button_GenereteSimlyInt";
+            button_GenereteSimlyInt.Size = new Size(190, 30);
+            button_GenereteSimlyInt.TabIndex = 0;
+            button_GenereteSimlyInt.Text = "Сгенерировать простые числа";
+            button_GenereteSimlyInt.UseVisualStyleBackColor = false;
+            button_GenereteSimlyInt.Click += button_GenereteSimlyInt_Click;
+            // 
+            // textBox_MinValue
+            // 
+            textBox_MinValue.Location = new Point(222, 6);
+            textBox_MinValue.Name = "textBox_MinValue";
+            textBox_MinValue.Size = new Size(85, 23);
+            textBox_MinValue.TabIndex = 1;
+            textBox_MinValue.KeyPress += textBox_MinValue_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(204, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Начать генерацию с числа:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(12, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(259, 21);
+            label2.TabIndex = 3;
+            label2.Text = "Верхняя граница числа генерации:";
+            // 
+            // textBox_MaxValue
+            // 
+            textBox_MaxValue.Location = new Point(277, 35);
+            textBox_MaxValue.Name = "textBox_MaxValue";
+            textBox_MaxValue.Size = new Size(81, 23);
+            textBox_MaxValue.TabIndex = 4;
+            textBox_MaxValue.KeyPress += textBox_MinValue_KeyPress;
+            // 
+            // listBox_ViewResult
+            // 
+            listBox_ViewResult.Dock = DockStyle.Bottom;
+            listBox_ViewResult.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox_ViewResult.FormattingEnabled = true;
+            listBox_ViewResult.ItemHeight = 21;
+            listBox_ViewResult.Location = new Point(0, 257);
+            listBox_ViewResult.Name = "listBox_ViewResult";
+            listBox_ViewResult.Size = new Size(800, 193);
+            listBox_ViewResult.TabIndex = 5;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            Controls.Add(listBox_ViewResult);
+            Controls.Add(textBox_MaxValue);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox_MinValue);
+            Controls.Add(button_GenereteSimlyInt);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "MainWindow";
             Text = "Многопоточность и асинхронность";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button button_GenereteSimlyInt;
+        private TextBox textBox_MinValue;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox_MaxValue;
+        private ListBox listBox_ViewResult;
     }
 }
--- MultithreadingAsynchrony/MainWindow.cs
+++ MultithreadingAsynchrony/MainWindow.cs
@@ -1,10 +1,81 @@
+using System.Threading;
+
 namespace MultithreadingAsynchrony
 {
     public partial class MainWindow : Form
     {
+
+        private Color GetRandomColor()
+        {
+            Random random = new Random();
+
+            random.Next();
+
+            return Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+
+        }
+
+        private void GenerateSimplyInt()
+        {
+            for (int i = 2; i < 50; i++)
+            {
+                if (i > 1)
+                {
+                    int count = 0;
+                    for (int j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0)
+                            count++;
+                    }
+
+                    if (count == 2)
+                    {
+                        listBox_ViewResult.Items.Add(i.ToString());
+                        listBox_ViewResult.SelectedIndex = listBox_ViewResult.Items.Count-1;
+                    }
+                }
+                Thread.Sleep(100);
+            }
+
+            listBox_ViewResult.Items.Add($"ÏÎÒÎÊ ÇÀÊÎÍ×ÅÍ ");
+        }
+
+        private List<Thread> _listTread = new List<Thread>();
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void button_GenereteSimlyInt_Click(object sender, EventArgs e)
+        {
+            if(_listTread.Count == 0)
+            {
+                listBox_ViewResult.Items.Clear();
+            }
+
+
+            Thread thread = new Thread(GenerateSimplyInt);
+            _listTread.Add(thread);
+
+            thread.Start();
+
+
+        }
+
+        private void textBox_MinValue_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
     }
+
+
+    public class InputedData
+    {
+        public int MinVal { get; set; }
+        public int MaxVal { get; set; }
+    }
+
+
 }
