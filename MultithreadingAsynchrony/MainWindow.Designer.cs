e14270671fa6f62481cbd4bfcb7da803239c5103
Viktor Velychko <Cezn.Vi@gmail.com>
2023-06-09 23:20:19 +0300

    WIP

--- MultithreadingAsynchrony/MainWindow.Designer.cs
+++ MultithreadingAsynchrony/MainWindow.Designer.cs
@@ -35,13 +35,25 @@
             label2 = new Label();
             textBox_MaxValue = new TextBox();
             listBox_ViewResult = new ListBox();
-            button_RestartTreads = new Button();
+            button_RestartTreadsSimplyInt = new Button();
+            label_textInfo = new Label();
+            label_description = new Label();
+            label_howManyThreads = new Label();
+            listBox_ViewFibonachi = new ListBox();
+            label_FibonachiInfo = new Label();
+            label3 = new Label();
+            label_CountThreadsFibonacy = new Label();
+            button_GenerateFib = new Button();
+            button_StopThreadsFibonacy = new Button();
+            button_PauseThreadsSimpleInt = new Button();
+            button_ResumeThreadsSimplInt = new Button();
+            button_RestartSimplyInt = new Button();
             SuspendLayout();
             // 
             // button_GenereteSimlyInt
             // 
             button_GenereteSimlyInt.BackColor = Color.LightGray;
-            button_GenereteSimlyInt.Location = new Point(168, 85);
+            button_GenereteSimlyInt.Location = new Point(12, 71);
             button_GenereteSimlyInt.Name = "button_GenereteSimlyInt";
             button_GenereteSimlyInt.Size = new Size(190, 30);
             button_GenereteSimlyInt.TabIndex = 0;
@@ -51,7 +63,7 @@
             // 
             // textBox_MinValue
             // 
-            textBox_MinValue.Location = new Point(222, 6);
+            textBox_MinValue.Location = new Point(170, 6);
             textBox_MinValue.Name = "textBox_MinValue";
             textBox_MinValue.Size = new Size(85, 23);
             textBox_MinValue.TabIndex = 1;
@@ -60,26 +72,26 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label1.Location = new Point(12, 9);
             label1.Name = "label1";
-            label1.Size = new Size(204, 21);
+            label1.Size = new Size(158, 15);
             label1.TabIndex = 2;
             label1.Text = "Начать генерацию с числа:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
             label2.Location = new Point(12, 37);
             label2.Name = "label2";
-            label2.Size = new Size(259, 21);
+            label2.Size = new Size(200, 15);
             label2.TabIndex = 3;
             label2.Text = "Верхняя граница числа генерации:";
             // 
             // textBox_MaxValue
             // 
-            textBox_MaxValue.Location = new Point(277, 35);
+            textBox_MaxValue.Location = new Point(218, 34);
             textBox_MaxValue.Name = "textBox_MaxValue";
             textBox_MaxValue.Size = new Size(81, 23);
             textBox_MaxValue.TabIndex = 4;
@@ -87,34 +99,160 @@
             // 
             // listBox_ViewResult
             // 
-            listBox_ViewResult.Dock = DockStyle.Bottom;
             listBox_ViewResult.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
             listBox_ViewResult.FormattingEnabled = true;
             listBox_ViewResult.ItemHeight = 21;
-            listBox_ViewResult.Location = new Point(0, 257);
+            listBox_ViewResult.Location = new Point(0, 252);
             listBox_ViewResult.Name = "listBox_ViewResult";
-            listBox_ViewResult.Size = new Size(800, 193);
+            listBox_ViewResult.Size = new Size(299, 256);
             listBox_ViewResult.TabIndex = 5;
             // 
-            // button_RestartTreads
+            // button_RestartTreadsSimplyInt
             // 
-            button_RestartTreads.Location = new Point(517, 169);
-            button_RestartTreads.Name = "button_RestartTreads";
-            button_RestartTreads.Size = new Size(261, 23);
-            button_RestartTreads.TabIndex = 6;
-            button_RestartTreads.Text = "Перазапустить потоки";
-            button_RestartTreads.UseVisualStyleBackColor = true;
-            button_RestartTreads.Click += button_RestartTreads_Click;
+            button_RestartTreadsSimplyInt.Location = new Point(9, 107);
+            button_RestartTreadsSimplyInt.Name = "button_RestartTreadsSimplyInt";
+            button_RestartTreadsSimplyInt.Size = new Size(193, 22);
+            button_RestartTreadsSimplyInt.TabIndex = 6;
+            button_RestartTreadsSimplyInt.Text = "Остановить потоки";
+            button_RestartTreadsSimplyInt.UseVisualStyleBackColor = true;
+            button_RestartTreadsSimplyInt.Click += button_RestartTreads_Click;
+            // 
+            // label_textInfo
+            // 
+            label_textInfo.AutoSize = true;
+            label_textInfo.Location = new Point(0, 218);
+            label_textInfo.Name = "label_textInfo";
+            label_textInfo.Size = new Size(175, 15);
+            label_textInfo.TabIndex = 7;
+            label_textInfo.Text = "Количество активных потоков";
+            // 
+            // label_description
+            // 
+            label_description.AutoSize = true;
+            label_description.Location = new Point(0, 236);
+            label_description.Name = "label_description";
+            label_description.Size = new Size(255, 15);
+            label_description.TabIndex = 8;
+            label_description.Text = "Результат работы генератора простых чисел";
+            // 
+            // label_howManyThreads
+            // 
+            label_howManyThreads.AutoSize = true;
+            label_howManyThreads.ForeColor = Color.Red;
+            label_howManyThreads.Location = new Point(181, 218);
+            label_howManyThreads.Name = "label_howManyThreads";
+            label_howManyThreads.Size = new Size(13, 15);
+            label_howManyThreads.TabIndex = 9;
+            label_howManyThreads.Text = "0";
+            // 
+            // listBox_ViewFibonachi
+            // 
+            listBox_ViewFibonachi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox_ViewFibonachi.FormattingEnabled = true;
+            listBox_ViewFibonachi.ItemHeight = 21;
+            listBox_ViewFibonachi.Location = new Point(314, 252);
+            listBox_ViewFibonachi.Name = "listBox_ViewFibonachi";
+            listBox_ViewFibonachi.Size = new Size(265, 256);
+            listBox_ViewFibonachi.TabIndex = 10;
+            // 
+            // label_FibonachiInfo
+            // 
+            label_FibonachiInfo.AutoSize = true;
+            label_FibonachiInfo.Location = new Point(314, 218);
+            label_FibonachiInfo.Name = "label_FibonachiInfo";
+            label_FibonachiInfo.Size = new Size(175, 15);
+            label_FibonachiInfo.TabIndex = 11;
+            label_FibonachiInfo.Text = "Количество активных потоков";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(314, 236);
+            label3.Name = "label3";
+            label3.Size = new Size(265, 15);
+            label3.TabIndex = 12;
+            label3.Text = "Результат работы генератора чисел Фибоначи\r\n";
+            // 
+            // label_CountThreadsFibonacy
+            // 
+            label_CountThreadsFibonacy.AutoSize = true;
+            label_CountThreadsFibonacy.ForeColor = Color.Red;
+            label_CountThreadsFibonacy.Location = new Point(495, 218);
+            label_CountThreadsFibonacy.Name = "label_CountThreadsFibonacy";
+            label_CountThreadsFibonacy.Size = new Size(13, 15);
+            label_CountThreadsFibonacy.TabIndex = 13;
+            label_CountThreadsFibonacy.Text = "0";
+            // 
+            // button_GenerateFib
+            // 
+            button_GenerateFib.BackColor = Color.LightGray;
+            button_GenerateFib.Location = new Point(314, 71);
+            button_GenerateFib.Name = "button_GenerateFib";
+            button_GenerateFib.Size = new Size(265, 30);
+            button_GenerateFib.TabIndex = 14;
+            button_GenerateFib.Text = "Сгенерировать числа Фибоначи";
+            button_GenerateFib.UseVisualStyleBackColor = false;
+            button_GenerateFib.Click += button_GenerateFib_Click;
+            // 
+            // button_StopThreadsFibonacy
+            // 
+            button_StopThreadsFibonacy.Location = new Point(315, 107);
+            button_StopThreadsFibonacy.Name = "button_StopThreadsFibonacy";
+            button_StopThreadsFibonacy.Size = new Size(264, 22);
+            button_StopThreadsFibonacy.TabIndex = 15;
+            button_StopThreadsFibonacy.Text = "Остановить потоки";
+            button_StopThreadsFibonacy.UseVisualStyleBackColor = true;
+            button_StopThreadsFibonacy.Click += button_StopThreadsFibonacy_Click;
+            // 
+            // button_PauseThreadsSimpleInt
+            // 
+            button_PauseThreadsSimpleInt.Location = new Point(9, 135);
+            button_PauseThreadsSimpleInt.Name = "button_PauseThreadsSimpleInt";
+            button_PauseThreadsSimpleInt.Size = new Size(193, 22);
+            button_PauseThreadsSimpleInt.TabIndex = 16;
+            button_PauseThreadsSimpleInt.Text = "Приостановить потоки";
+            button_PauseThreadsSimpleInt.UseVisualStyleBackColor = true;
+            // 
+            // button_ResumeThreadsSimplInt
+            // 
+            button_ResumeThreadsSimplInt.Location = new Point(9, 163);
+            button_ResumeThreadsSimplInt.Name = "button_ResumeThreadsSimplInt";
+            button_ResumeThreadsSimplInt.Size = new Size(193, 22);
+            button_ResumeThreadsSimplInt.TabIndex = 17;
+            button_ResumeThreadsSimplInt.Text = "Востановить потоки";
+            button_ResumeThreadsSimplInt.UseVisualStyleBackColor = true;
+            // 
+            // button_RestartSimplyInt
+            // 
+            button_RestartSimplyInt.Location = new Point(9, 191);
+            button_RestartSimplyInt.Name = "button_RestartSimplyInt";
+            button_RestartSimplyInt.Size = new Size(193, 22);
+            button_RestartSimplyInt.TabIndex = 18;
+            button_RestartSimplyInt.Text = "Рестарт";
+            button_RestartSimplyInt.UseVisualStyleBackColor = true;
+            button_RestartSimplyInt.Click += button_RestartSimplyInt_Click;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button_RestartTreads);
-            Controls.Add(listBox_ViewResult);
+            ClientSize = new Size(580, 520);
+            Controls.Add(button_RestartSimplyInt);
+            Controls.Add(button_ResumeThreadsSimplInt);
+            Controls.Add(button_PauseThreadsSimpleInt);
+            Controls.Add(button_StopThreadsFibonacy);
+            Controls.Add(button_GenerateFib);
+            Controls.Add(label_CountThreadsFibonacy);
+            Controls.Add(label3);
+            Controls.Add(label_FibonachiInfo);
+            Controls.Add(listBox_ViewFibonachi);
+            Controls.Add(button_RestartTreadsSimplyInt);
             Controls.Add(textBox_MaxValue);
+            Controls.Add(label_howManyThreads);
             Controls.Add(label2);
+            Controls.Add(label_description);
+            Controls.Add(label_textInfo);
+            Controls.Add(listBox_ViewResult);
             Controls.Add(label1);
             Controls.Add(textBox_MinValue);
             Controls.Add(button_GenereteSimlyInt);
@@ -133,6 +271,18 @@
         private Label label2;
         private TextBox textBox_MaxValue;
         private ListBox listBox_ViewResult;
-        private Button button_RestartTreads;
+        private Button button_RestartTreadsSimplyInt;
+        private Label label_textInfo;
+        private Label label_description;
+        private Label label_howManyThreads;
+        private ListBox listBox_ViewFibonachi;
+        private Label label_FibonachiInfo;
+        private Label label3;
+        private Label label_CountThreadsFibonacy;
+        private Button button_GenerateFib;
+        private Button button_StopThreadsFibonacy;
+        private Button button_PauseThreadsSimpleInt;
+        private Button button_ResumeThreadsSimplInt;
+        private Button button_RestartSimplyInt;
     }
 }
--- MultithreadingAsynchrony/MainWindow.cs
+++ MultithreadingAsynchrony/MainWindow.cs
@@ -2,7 +2,6 @@ namespace MultithreadingAsynchrony
 {
     public partial class MainWindow : Form
     {
-
         private Color GetRandomColor()
         {
             Random random = new Random();
@@ -13,11 +12,23 @@ namespace MultithreadingAsynchrony
 
         }
 
-        private void GenerateSimplyInt()
-        {
+        private List<InputedData> _list = new List<InputedData>();
+        private List<InputedData> _listFibonaci = new List<InputedData>();
 
-            for (int i = 2; i < 50; i++)
+
+        private void GenerateSimplyInt(object obj)
+        {
+            if (obj is null)
+                return;
+
+            InputedData data = (InputedData)obj;
+
+            for (int i = data.MinVal; i < data.MaxVal; i++)
             {
+
+                if (data.IsEnded == true)
+                    break;
+
                 if (i > 1)
                 {
                     int count = 0;
@@ -29,40 +40,147 @@ namespace MultithreadingAsynchrony
 
                     if (count == 2)
                     {
-                        listBox_ViewResult.Invoke(() => listBox_ViewResult.Items.Add(i));
-
-
-                        //listBox_ViewResult.Items.Add(i.ToString());
-
-
-
-                        //listBox_ViewResult.SelectedIndex = listBox_ViewResult.Items.Count - 1;
+                        listBox_ViewResult.Items.Add(i.ToString());
                     }
                 }
+
                 Thread.Sleep(100);
+                listBox_ViewResult.SelectedIndex = listBox_ViewResult.Items.Count - 1;
+
             }
+            data.IsEnded = true;
+            HowManySimplyIntThreads();
+            listBox_ViewResult.Items.Add($"ÏÎÒÎÊ {Thread.CurrentThread.Name} ÇÀÊÎÍ×ÅÍ ");
+            listBox_ViewResult.SelectedIndex = listBox_ViewResult.Items.Count - 1;
 
-
-            listBox_ViewResult.Items.Add($"ÏÎÒÎÊ ÇÀÊÎÍ×ÅÍ ");
         }
 
-        private List<Thread> _listTread = new List<Thread>();
+
+        private void GenerateFibonacyInt(object obj)
+        {
+            if (obj is null)
+                return;
+
+            InputedData data = (InputedData)obj;
+
+            int[] array = new int[40];
+
+
+            for (int i = 0; i < 40; ++i)
+            {
+                if (data.IsEnded == true) { break; }
+                array[i] = i < 2 ? 1 : array[i - 2] + array[i - 1];
+
+                listBox_ViewFibonachi.Items.Add(array[i]);
+
+                Thread.Sleep(100);
+
+                listBox_ViewFibonachi.SelectedIndex = listBox_ViewFibonachi.Items.Count - 1;
+            }
+
+            data.IsEnded = true;
+            HowManyFibIntThreads();
+            listBox_ViewFibonachi.Items.Add($"ÏÎÒÎÊ {Thread.CurrentThread.Name} ÇÀÊÎÍ×ÅÍ ");
+            listBox_ViewFibonachi.SelectedIndex = listBox_ViewFibonachi.Items.Count - 1;
+
+        }
+
+        private void HowManySimplyIntThreads()
+        {
+            int count = 0;
+
+            foreach (InputedData item in _list)
+            {
+                if (item.IsEnded == false)
+                    count++;
+            }
+
+            label_howManyThreads.Text = count.ToString();
+        }
+
+        private void HowManyFibIntThreads()
+        {
+            int count = 0;
+
+            foreach (InputedData item in _listFibonaci)
+            {
+                if (item.IsEnded == false)
+                    count++;
+            }
+
+            label_CountThreadsFibonacy.Text = count.ToString();
+        }
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Ãåíåðèðóåò ïðîñòûå ÷èñëà
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button_GenereteSimlyInt_Click(object sender, EventArgs e)
         {
+            InputedData inputedData;
 
-            Thread thread = new Thread(GenerateSimplyInt);
-            _listTread.Add(thread);
+            /////////////ÏÐÎÂÅÐÊÀ
+            if (textBox_MaxValue.Text.Length > 0 &&
+                textBox_MinValue.Text.Length > 0 &&
+                textBox_MaxValue.Text.Length <= 8 &&
+                textBox_MinValue.Text.Length <= 8
+                )
+                inputedData = new InputedData()
+                {
+                    IsEnded = false,
+                    IsPaused = false,
+                    MaxVal = Int32.Parse(textBox_MaxValue.Text),
+                    MinVal = Int32.Parse(textBox_MinValue.Text),
+                };
+            else
+                inputedData = new InputedData()
+                {
+                    IsEnded = false,
+                    IsPaused = false,
+                    MaxVal = 0,
+                    MinVal = 0,
+                };
+            /////////////ÏÐÎÂÅÐÊÀ----ÊÎÍÅÖ
 
-            thread.Start();
+            textBox_MaxValue.Clear();
+            textBox_MinValue.Clear();
 
+            _list.Add(inputedData);
 
+            Thread thread = new Thread(new ParameterizedThreadStart(GenerateSimplyInt));
+            thread.Name = $"{DateTime.Now.Millisecond}";
+
+            thread.Start(inputedData);
+            HowManySimplyIntThreads();
         }
+
+        private void button_GenerateFib_Click(object sender, EventArgs e)
+        {
+            InputedData inputedData = new InputedData()
+            {
+                IsEnded = false,
+                IsPaused = false,
+                MaxVal = 0,
+                MinVal = 0,
+            };
+
+            _listFibonaci.Add(inputedData);
+
+            Thread thread = new Thread(new ParameterizedThreadStart(GenerateFibonacyInt));
+            thread.Name = $"{DateTime.Now.Second}{DateTime.Now.Millisecond}";
+
+            thread.Start(inputedData);
+
+            HowManyFibIntThreads();
+        }
+
+
 
         private void textBox_MinValue_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -71,18 +189,68 @@ namespace MultithreadingAsynchrony
 
         private void button_RestartTreads_Click(object sender, EventArgs e)
         {
-            foreach (Thread item in _listTread)
+            foreach (InputedData inputedData in _list)
             {
-                listBox_ViewResult.Text = ($"ÏÎÒÎÊ: ( {item.Name} ), îí æèâ> {item.IsAlive}");
+                inputedData.IsEnded = true;
+
             }
+        }
+
+        private void button_StopThreadsFibonacy_Click(object sender, EventArgs e)
+        {
+            foreach (InputedData inputedData in _listFibonaci)
+            {
+                inputedData.IsEnded = true;
+
+            }
+        }
+
+        private void button_RestartSimplyInt_Click(object sender, EventArgs e)
+        {
+            foreach (InputedData inputedData in _list)
+            {
+                inputedData.IsEnded = true;
+
+            }
+            _list.Clear();
         }
     }
 
 
     public class InputedData
     {
-        public int MinVal { get; set; }
-        public int MaxVal { get; set; }
+        private int _minVal;
+        private int _maxVal;
+
+
+
+        public int MinVal
+        {
+            get { return _minVal; }
+            set
+            {
+                if (value < 2 && value > int.MaxValue)
+                    _minVal = 2;
+                else
+                    _minVal = value;
+            }
+        }
+        public int MaxVal
+        {
+            get { return _maxVal; }
+
+            set
+            {
+                if (value < 2 || value > int.MaxValue)
+                    _maxVal = int.MaxValue;
+                else
+                    _maxVal = value;
+
+            }
+        }
+
+        public bool IsPaused { get; set; }
+        public bool IsEnded { get; set; }
     }
 
 
